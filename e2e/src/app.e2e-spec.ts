8565a10c96bc0ab12425f6681d9fad53b05c3363
muadz <goadzaskarul@gmail.com>
2020-09-13 14:48:32 +0700

    change app name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-gh-pages app is running!');
+    expect(page.getTitleText()).toEqual('agung-sari-baja-web app is running!');
   });
 
   afterEach(async () => {
